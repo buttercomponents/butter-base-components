357905790099bba714d22523748941e099b6a624
Niv Sardi <xaiki@evilgiggle.com>
2017-06-21 16:14:49 -0300

    modal: switch to class
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -3,14 +3,20 @@ import { translate } from 'react-i18next';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 import style from './style.styl';
 
-let Modal = ({position, action, children}) => (
-    <div className={style.modal} data-position={position} key={0}>
-        <div className={style.widget}>
-            {children}
-        </div>
-        <div className={style.overlay} onClick={action.apply}></div>
-    </div>
-)
+class Modal extends Component {
+    render () {
+        let {props} = this
+        let {position, action, children} = props
+        return (
+            <div className={style.modal} data-position={position} key={0}>
+                <div className={style.widget}>
+                    {children}
+                </div>
+                <div className={style.overlay} onClick={action.apply}></div>
+            </div>
+        )
+    }
+}
 
 class ButterModal extends Component {
     render () {
