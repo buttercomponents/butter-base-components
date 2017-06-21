02fd82a3756e28249e6255fa007284d99f51d189
Niv Sardi <xaiki@evilgiggle.com>
2017-06-21 13:54:03 -0300

    add modals
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ src/components/modal/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import style from './style.styl';
+
+let Modal = ({position, action, children}) => (
+    <div className={style.modal} data-position={position} key={0}>
+        <div className={style.widget}>
+            {children}
+        </div>
+        <div className={style.overlay} onClick={action.apply}></div>
+    </div>
+)
+
+class ButterModal extends Component {
+    render () {
+        let {show, ...props} = this.props
+        return (
+            <CSSTransitionGroup
+                transitionName="popup"
+                transitionAppear={true}
+                transitionAppearTimeout={200}
+                transitionEnterTimeout={200}
+                transitionLeaveTimeout={200}>
+                {show?<Modal {...props}/>:null}
+            </CSSTransitionGroup>
+        )
+    }
+}
+
+export {
+    translate(['modal'], {wait: true, withRef: true})(Modal),
+    translate(['butter-modal'], {wait: true, withRef: true})(ButterModal)
+}
--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import Alert        from './components/alert'
 import Buttons      from './components/button'
 import Dropdowns    from './components/dropdown'
 import Switch       from './components/switch'
+import Modals       from './components/modal'
 
-export {ActionBar, Alert, Buttons, Dropdowns, Switch}
+export {ActionBar, Alert, Buttons, Dropdowns, Modals, Switch}
