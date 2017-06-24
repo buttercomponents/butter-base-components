2b998a1db6e3a38d864e1d4375fcce359354e81e
Baltazar Gomez <btzr.io@gmail.com>
2017-06-24 15:03:31 -0600

    add button component for action bar

--- src/components/action-bar/index.js
+++ src/components/action-bar/index.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import style from './style.styl';
 
+let button = ({...props}) => (
+    <li>
+        <i data-toggle="tooltip" data-placement="left" title={t(title)} className="material-icons">{icon}</i>
+    </li>
+)
+
 let ActionBar = ({...props}) => (
     <div id="action-bar" className={style['action-bar']}>
         <div className={style['buttons']}>
-            <li>
-                <i className="material-icons">link</i>
-            </li>
-            <li>
-                <i className="material-icons">fiber_manual_record</i>
-            </li>
+            {"..."}
         </div>
     </div>
 )
