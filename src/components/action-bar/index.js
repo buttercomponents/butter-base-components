f0e47e792289063e071e6cd03486906546afbd79
btzr-io <btzr.io@gmail.com>
2017-06-25 22:25:38 -0600

    implement action-bar component

--- src/components/action-bar/index.js
+++ src/components/action-bar/index.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import style from './style.styl';
 
+const Styles = (a) => a.filter(b => !!b).join(' ');
 
-let button = (props) => (
-    <li>
-        <i data-toggle="tooltip" data-placement="left" title={props.title} className="material-icons">{props.icon}</i>
+let Button = ({...props}) => (
+    <li className={Styles([style.button, props.active && style.active])} onClick={props.action}>
+        <i className="material-icons">{props.icon}</i>
     </li>
 )
 
 let ActionBar = ({...props}) => (
     <div id="action-bar" className={style['action-bar']}>
-        <div className={style['buttons']}>
-        </div>
+        <ul className={style['buttons']}>
+            {props.buttons.map((i, k) => <Button key={k} {...i}/> )}
+        </ul>
     </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {translate} from 'react-i18next';
+import ActionBar from './components/action-bar';
+import style from './styl/style.styl';
 
-import ActionBar    from './components/action-bar'
-import Alert        from './components/alert'
-import Buttons      from './components/button'
-import Dropdowns    from './components/dropdown'
-import Switch       from './components/switch'
-import Modal        from './components/modal'
 
-export {ActionBar, Alert, Buttons, Dropdowns, Modal, Switch}
+
+let items = [
+    {title: "button-01", icon:"start"},
+    {title: "button-02", icon:"favorite", active: true},
+    {title: "button-03", icon:"link", active: false},
+    {title: "button-04", icon:"settings", active: true}
+];
+
+let Test = ({test, t}) => (
+    <div className={style.layout}>
+        <ActionBar buttons={items} />
+    </div>
+)
+
+export default translate('test')(Test)
