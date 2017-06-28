f5341b47c7bb7fa4e413b0d34748d210378629f7
Niv Sardi <xaiki@evilgiggle.com>
2017-06-28 03:36:11 -0300

    fixup for ActionBar -> ToolBar
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
-import ActionBar    from './src/components/action-bar'
+import ToolBar      from './src/components/toolbar'
 import Alert        from './src/components/alert'
 import Buttons      from './src/components/button'
 import Dropdowns    from './src/components/dropdown'
 import Switch       from './src/components/switch'
 import Modal        from './src/components/modal'
 
-export {ActionBar, Alert, Buttons, Dropdowns, Modal, Switch}
+// XXX: backward compat
+let ActionBar = ToolBar
+
+export {ActionBar, ToolBar, Alert, Buttons, Dropdowns, Modal, Switch}
