5b68364cf53f3a055d6c4feaed933aeb4d93bf6f
btzr-io <btzr.io@gmail.com>
2018-04-09 23:15:48 -0600

    rewrite stars component -> react.class

--- src/components/stars/index.js
+++ src/components/stars/index.js
@@ -2,38 +2,63 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './style.styl';
 
-function makeStars(count, max=5) {
-    let ret = []
+function getScore(count, max=5) {
     const top =  count <= 10 ? 10 : 100;
-    const score = (max * count) / top;
-    const hasHalfStar = score % 1 >= 0.5;
-    const truncScore = Math.trunc(score);
-    const emptyStars = max - truncScore - ( hasHalfStar ? 1 : 0 );
-
-    for (let i = 0; i < truncScore; i += 1) {
-        ret.push(<i key={`full-${i}`} className={`material-icons ${style.starFull}`}>star</i>)
-    }
-
-    hasHalfStar && ret.push(<i key={'half-star-${score}'} className={`material-icons ${style.starFull}`}>star_half</i>)
-
-    for (let i = 0; i < emptyStars; i += 1) {
-        ret.push(<i key={`empty-${i}`} className={`material-icons ${style.starFull}`}>star_border</i>)
-    }
-    return ret
+    return (max * count) / top;
 }
 
-let Stars = ({rating}) => (
-    <div className={`${style.stars}`}>
-        {makeStars(rating)}
-    </div>
-)
+class Stars extends React.Component{
+    constructor(props) {
+        super(props)
+        const { rating, max } = this.props;
+        this.state = {
+             score: getScore(rating, max)
+         }
+    }
+
+    getStars() {
+        const stars = [];
+        const { score } = this.state;
+        const { max } = this.props;
+        const hasHalfStar = score % 1 >= 0.5;
+        const truncScore = Math.trunc(score);
+        const emptyStars = max - truncScore - ( hasHalfStar ? 1 : 0 );
+
+        for (let i = 0; i < truncScore; i += 1) {
+            stars.push(<i key={`full-${i}`} className={`material-icons ${style.starFull}`}>star</i>)
+        }
+
+        hasHalfStar && stars.push(<i key={'half-star-${score}'} className={`material-icons ${style.starFull}`}>star_half</i>)
+
+        for (let i = 0; i < emptyStars; i += 1) {
+            stars.push(<i key={`empty-${i}`} className={`material-icons ${style.starFull}`}>star_border</i>)
+        }
+
+        return stars
+    }
+
+    render() {
+        const { score } = this.state;
+        const { rating, max } = this.props;
+        
+        return (
+            <div className={`${style.stars}`} title={`${score} / ${max}`}>
+                {this.getStars(rating, max)}
+            </div>
+        )
+    }
+}
+
+
 
 Stars.propTypes = {
-    rating: PropTypes.number.isRequired
+    rating: PropTypes.number.isRequired,
+    max: PropTypes.number,
 }
 
 Stars.defaultProps = {
-    rating: 0
+    rating: 0,
+    max: 5,
 }
 
 export {Stars as default}
