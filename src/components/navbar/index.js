2aecfad0b3918a2a4228ab0fa9440fbcb6b45b47
Niv Sardi <xaiki@evilgiggle.com>
2018-05-10 11:59:38 -0300

    newer gobackbutton code
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,23 +4,29 @@ import { translate } from 'react-i18next';
 import Toolbar from '../toolbar';
 import style from './style.styl';
 
-let GoBackButton = ({...props}) => (
-    <a className={style.button} onClick={props.action}>
+const Identity = (a) => (a)
+
+let GoBackButton = ({action = Identity, title}) => (
+    <a className={style.button} onClick={action}>
         <i className="material-icons">arrow_back</i>
+        {title ? title : null}
     </a>
 )
 
-let Navbar = ({goBack, title, right, left, ...props}) => (
+GoBackButton.propTypes = {
+    action: PropTypes.func.isRequired,
+}
+
+let Navbar = ({goBack, title, right, left}) => (
     <nav id="navbar" className={style.navbar}>
         <div className={style.menu}>
-            {goBack && <GoBackButton action={goBack}/>}
+            {goBack && <GoBackButton {...goBack}/>}
             {left && left}
             {title && <h1 className={style.title}>{title}</h1>}
         </div>
         {right && right}
     </nav>
 )
-
 
 Navbar.propTypes = {
     title: PropTypes.string,
