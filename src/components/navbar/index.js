1ed9e0e52a2570d124377853587b05be12286469
Niv Sardi <xaiki@evilgiggle.com>
2018-05-13 05:10:19 -0300

    navbar: let ⇒ const
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,7 +6,7 @@ import style from './style.styl';
 
 const Identity = (a) => (a)
 
-let GoBackButton = ({action = Identity, title}) => (
+const GoBackButton = ({action = Identity, title}) => (
     <a className={style.button} onClick={action}>
         <i className="material-icons">arrow_back</i>
         {title ? <span>{title}</span> : null}
@@ -17,8 +17,8 @@ GoBackButton.propTypes = {
     action: PropTypes.func.isRequired,
 }
 
-let Navbar = ({goBack, title, right, left}) => (
-    <nav id="navbar" className={style.navbar}>
+const Navbar = ({goBack, title, right, left, type}) => (
+    <nav id="navbar" className={`${style.navbar} ${type}`}>
         <div className={style.menu}>
             {goBack && <GoBackButton {...goBack}/>}
             {left && left}
@@ -33,7 +33,7 @@ Navbar.propTypes = {
     goBack: PropTypes.func
 }
 
-let Translated = translate(['navbar'])(Navbar)
+const Translated = translate(['navbar'])(Navbar)
 
 export {
     Translated as default,
