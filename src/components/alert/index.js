af825348f3162bd97d8c114183b35a16efe65ae0
Niv Sardi <xaiki@evilgiggle.com>
2018-05-17 16:02:39 -0300

    alert: makeit a functional component
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { translate } from 'react-i18next';
 import style from './style.styl';
 
-class Alert extends Component {
-    render () {
-        let {props} = this
-
-        return (
-            <div className={style['success']}>
-                <span>{props.t(props.message)}</span>
-                <i className="material-icons">check</i>
-            </div>
-        )
-    }
-}
+const Alert = ({t, message}) => (
+    <div className={style['success']}>
+        <span>{t(message)}</span>
+        <i className="material-icons">check</i>
+    </div>
+)
 
 export default translate(['alert'])(Alert);
