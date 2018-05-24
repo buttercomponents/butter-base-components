2faac9471b3caac96f208787d298a3680f2a4d3d
Niv Sardi <xaiki@evilgiggle.com>
2018-05-24 04:21:04 -0300

    search should be a functional component
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -2,24 +2,11 @@ import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import style from './style.styl';
 
-class Search extends Component {
-
-    constructor (props) {
-        super()
-        this.state = {}
-    }
-
-    render () {
-        let {props, state} = this
-        return (
-            <div id="search" className={style.search}>
-                <input type="text" className={style.input} placeholder="Search..."/>
-                <i className="material-icons">search</i>
-            </div>
-        )
-    }
-
-}
-
+const Search = () => (
+    <div id="search" className={style.search}>
+        <input type="text" className={style.input} placeholder="Search..."/>
+        <i className="material-icons">search</i>
+    </div>
+)
 
 export default translate(['search'])(Search);
