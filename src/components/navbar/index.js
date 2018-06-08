adf3aa6a1fdfc1dbaaaf4186faa974234be7f138
Niv Sardi <xaiki@evilgiggle.com>
2018-06-08 10:32:15 -0300

    navbar: allow for only one child without cruft
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -21,18 +21,35 @@ GoBackButton.defaultProps = {
     title: 'back'
 }
 
-const Navbar = ({goBack, title, right, children, left, type}) => (
-    <nav id="navbar" className={`${style.navbar} ${type ? type : ''}`}>
-        <div className={style.menu} role='navbar-left'>
-            {goBack && <GoBackButton {...goBack}/>}
-            {left}
-            {title && <h1 className={style.title}>{title}</h1>}
-        </div>
-        {children && <div role='navbar-center'>{children}</div>}
-        {(children || right) && <div role='navbar-right'>{right}</div>}
-
+const OneChildNavbar = ({children, type, show}) => (
+    <nav id="navbar" className={`${style.navbar} ${type ? type : ''}`} show={show?"true":"false"}>
+        {children}
     </nav>
 )
+
+const ComplexNavbar = ({goBack, title, right, children, left}) => [
+    <div className={style.menu} role='navbar-left' key='left'>
+        {goBack && <GoBackButton {...goBack}/>}
+        {left}
+        {title && <h1 className={style.title}>{title}</h1>}
+    </div>,
+    children ? <div key='center' role='navbar-center'>{children}</div>: null,
+    (children || right) ? <div key='right' role='navbar-right'>{right}</div> : null
+]
+
+const Navbar = ({goBack, title, right, children, left, type, show}) => {
+    const complexProps = {goBack, title, right, children, left}
+
+    return (
+        <nav id="navbar"
+             className={`${style.navbar} ${type ? type : ''}`}
+             visible={show?"true":"false"}>
+            {(goBack || title || right || left) ?
+             <ComplexNavbar {...complexProps}/>
+             : children}
+        </nav>
+    )
+}
 
 Navbar.propTypes = {
     title: PropTypes.string,
@@ -43,5 +60,5 @@ const Translated = translate(['navbar'])(Navbar)
 
 export {
     Translated as default,
-    GoBackButton
+    GoBackButton,
 }
