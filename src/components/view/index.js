f776c9b97b36bde0ea800af3dabe31479124bbdc
Niv Sardi <xaiki@evilgiggle.com>
2018-06-08 10:32:36 -0300

    view: convert to functional component
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/view/index.js
+++ src/components/view/index.js
@@ -3,28 +3,16 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import style from './style.styl';
 
-class View extends Component {
+const View = ({children}) => (
+    <div className={style.view} ref={node => node && node.addEventListener('scroll', e => console.log('scroll outter', e))}>
+        <div className={style.content} ref={node => node && node.addEventListener('scroll', e => console.log('scroll inner', e))}>
+            {children}
+        </div>
+    </div>
+)
 
-    static propTypes = {
-        navbar: PropTypes.object
-    }
-
-    constructor (props) {
-        super(props)
-    }
-
-    render () {
-        let {props, state} = this
-        return (
-            <div className={style.view}>
-                <div className={style.content}>
-                    {props.children}
-                </div>
-            </div>
-        )
-    }
+View.propTypes = {
+    navbar: PropTypes.object
 }
-
-
 
 export {View as default}
